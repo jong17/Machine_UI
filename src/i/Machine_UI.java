1198d2c4881b16da898729114b7ef7740e445d93
jong17 <faisol.lateh@gmail.com>
2015-05-26 14:19:11 +0700

    only test

--- src/i/Machine_UI.java
+++ src/i/Machine_UI.java
@@ -16,6 +16,7 @@ public class Machine_UI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println('Test git hub');
     }
     
 }
